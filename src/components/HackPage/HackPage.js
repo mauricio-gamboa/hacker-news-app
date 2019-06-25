7670f1fcf4426ddd6dd9cc6e24d7f25ae9735d75
Mauricio <magalocr@gmail.com>
2019-06-25 02:04:59 -0600

    Error message if hack is not found.

--- src/components/HackPage/HackPage.js
+++ src/components/HackPage/HackPage.js
@@ -15,6 +15,10 @@ function HackPage({ match }) {
     const { id } = match.params;
     const [hack] = useState(() => getOneFromStorage(id));
 
+    if (!hack) {
+        return(<h2>404 Hack cannot be found 404</h2>);
+    }
+
     return (
         <div className='page hackPage'>
             <h1>

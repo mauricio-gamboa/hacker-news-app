e866afd842979c8cdda02ea6cc22103f82d5ce98
Mauricio <magalocr@gmail.com>
2019-06-25 19:19:05 -0600

    Added tests to the LoadMoreButton component.

--- src/components/LoadMoreButton/LoadMoreButton.test.js
+++ src/components/LoadMoreButton/LoadMoreButton.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
+
 import LoadMoreButton from './LoadMoreButton';
 
 describe('LoadMoreButton', () => {
@@ -7,5 +9,19 @@ describe('LoadMoreButton', () => {
         const div = document.createElement('div');
         ReactDOM.render(<LoadMoreButton />, div);
         ReactDOM.unmountComponentAtNode(div);
+    });
+
+    test('reders the correct text', () => {
+        const text = 'Werkspot is really cool.'
+        const { getByText } = render(<LoadMoreButton>{text}</LoadMoreButton>);
+        expect(getByText(text)).toBeInTheDocument();
+    });
+
+    test('calls the handleClick prop', () => {
+        const handleClickMock = jest.fn();
+        const { container } = render(<LoadMoreButton handleClick={handleClickMock} />);
+        const button = container.firstChild;
+        fireEvent.click(button);
+        expect(handleClickMock).toHaveBeenCalledTimes(1);
     });
 });
